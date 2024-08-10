e940972531c16f8093de4176e9e23a2b2e1b89a8
Roman Burlaka <burlakarv@gmail.com>
2024-08-10 19:37:54 +0200

    MostWantedPage

--- src/test/java/automation/MostWantedPageTest.java
+++ src/test/java/automation/MostWantedPageTest.java
@@ -1,4 +1,31 @@
 package automation;
 
-public class MostWantedPageTest {
+import automation.model.HomePage;
+import automation.runner.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MostWantedPageTest extends BaseTest {
+
+    @Test
+    public void testCheckCurrentPage() {
+        int subStringUrl = new HomePage(driver)
+                .goToMostWantedPage()
+                .showUrlIndexOf();
+        Assert.assertEquals(subStringUrl, 37);
+
+        boolean subStringUrlContains = new HomePage(driver)
+                .goToMostWantedPage()
+                .showUrlContains();
+        Assert.assertTrue(subStringUrlContains);
+
+        int subStringUtilsIgnoreCase = new HomePage(driver)
+                .goToMostWantedPage()
+                .showUrlStringUtilsContainsIgnoreCase();
+        System.out.println(subStringUtilsIgnoreCase);
+
+
+    }
+
+
 }
--- src/test/java/automation/runner/BaseTest.java
+++ src/test/java/automation/runner/BaseTest.java
@@ -28,7 +28,6 @@ abstract public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         getWeb();
     }
 
